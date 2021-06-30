e4614d14c9923a150d9b30b48b34b61fe9722a2f
Luis <luisreyes84@live.com>
2021-06-30 00:26:17 -0700

    added single line and multiline comments, and name and food to log

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Hello World! This is my single line comment
+
+// Hello World! This is my multi-line comment but I realize I need to add more content in order to make this fill up two lines
+
+let firstName = 'Luis';
+console.log(firstName);
+
+let food = 'Chili Cheese Fries';
+console.log(food);
