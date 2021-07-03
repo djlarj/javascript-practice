d7453d799f75deb39330c9b1202fde37a0ff6c2b
Luis <luisreyes84@live.com>
2021-07-02 18:07:20 -0700

    Completed Day 2 Challenge

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Day 1
+
 // Hello World! This is my single line comment
 
 // Hello World! This is my multi-line comment but I realize I need to add more content in order to make this fill up two lines
@@ -7,3 +9,18 @@ console.log(firstName);
 
 let food = 'Chili Cheese Fries';
 console.log(food);
+
+
+// Day 2
+
+const stringLength = 'Supercalifragilisticexpialidocious';
+console.log(`${stringLength.length}`);
+console.log(typeof `${stringLength.length}`)
+
+let n = 3.14159265359;
+console.log(n);
+console.log(typeof n)
+
+const bigint = 1234567890123456789012345678901234567890n;
+console.log(bigint);
+console.log(typeof bigint)
