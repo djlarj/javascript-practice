ef9cbae52571ffc6f95183e5c3a3c1c5353c34bc
Luis <luisreyes84@live.com>
2021-07-06 17:55:13 -0700

    fixed day 1 multi-line comment.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 // Hello World! This is my single line comment
 
-// Hello World! This is my multi-line comment but I realize I need to add more content in order to make this fill up two lines
+/* Hello World! This is my multi-line comment but I realize I need to add more content in order to make this fill up two lines */
 
 let firstName = 'Luis';
 console.log(firstName);
